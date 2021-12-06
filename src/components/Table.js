31b9ca94cde06f6b96087530e156ed6e43a7795b
Gustavo <fariaguga@hotmail.com>
2021-12-06 17:43:54 -0200

    Refatoração Requisito 1

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,7 @@
-import React, { useContext } from 'react';
-import PlanetContext from '../context/PlanetContext';
+import React from 'react';
+import BodyTable from './BodyTable';
 
 function Table() {
-  const { data } = useContext(PlanetContext);
-  // console.log(data);
   return (
     <table>
       <thead>
@@ -23,25 +21,7 @@ function Table() {
           <th>Url</th>
         </tr>
       </thead>
-      <tbody>
-        { data.map((elemento) => (
-          <tr key={ elemento.name }>
-            <td>{ elemento.name }</td>
-            <td>{ elemento.rotation_period }</td>
-            <td>{ elemento.orbital_period }</td>
-            <td>{ elemento.diameter }</td>
-            <td>{ elemento.climate }</td>
-            <td>{ elemento.gravity }</td>
-            <td>{ elemento.terrain }</td>
-            <td>{ elemento.surface_water }</td>
-            <td>{ elemento.population }</td>
-            <td>{ elemento.films }</td>
-            <td>{ elemento.created }</td>
-            <td>{ elemento.edited }</td>
-            <td>{ elemento.url }</td>
-          </tr>
-        ))}
-      </tbody>
+      <BodyTable />
     </table>
   );
 }
